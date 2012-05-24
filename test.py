498db7dd0702efc86adb1054bce1731fcd0aefd6
Pedro Rodrigues <medecau@gmail.com>
2012-05-24 12:23:27 +0100

    moved mathkit tests to test.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import crypto
 import hashlib
 import diff
 import difflib
+import mathkit
 import stringutil
 
 class testCrypto(unittest.TestCase):
@@ -64,6 +65,80 @@ class testStringutil(unittest.TestCase):
     def test_to_ascii(self):
         self.assertEqual(stringutil.to_ascii(self.u), 'aeiouiuc')
 
+
+
+class testMathkit(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_is_prime_1(self):
+        self.assertTrue(mathkit.is_prime(2))
+
+    def test_is_prime_2(self):
+        self.assertTrue(mathkit.is_prime(11))
+
+    def test_is_prime_3(self):
+        self.assertFalse(mathkit.is_prime(0))
+
+    def test_is_prime_4(self):
+        self.assertFalse(mathkit.is_prime(1))
+
+    def test_is_prime_5(self):
+        self.assertFalse(mathkit.is_prime(-1))
+
+    def test_is_prime_6(self):
+        self.assertFalse(mathkit.is_prime(9))
+
+    def test_next_prime_1(self):
+        self.assertEqual(mathkit.next_prime(-1), 2)
+
+    def test_next_prime_2(self):
+        self.assertEqual(mathkit.next_prime(1), 2)
+
+    def test_next_prime_3(self):
+        self.assertEqual(mathkit.next_prime(2), 3)
+
+    def test_next_prime_4(self):
+        self.assertEqual(mathkit.next_prime(3), 5)
+
+    def test_next_prime_5(self):
+        self.assertEqual(mathkit.next_prime(9), 11)
+
+    def test_get_factors_1(self):
+        factors=mathkit.get_factors(1)
+        self.assertEqual(factors, [1])
+
+    def test_get_factors_2(self):
+        factors=mathkit.get_factors(2)
+        self.assertEqual(factors, [1,2])
+
+    def test_get_factors_3(self):
+        factors=mathkit.get_factors(3)
+        self.assertEqual(factors, [1,3])
+
+    def test_get_factors_4(self):
+        factors=mathkit.get_factors(8)
+        factors.sort()
+        self.assertEqual(factors, [1,2,4,8])
+
+    def test_get_factors_5(self):
+        factors=mathkit.get_factors(25)
+        factors.sort()
+        self.assertEqual(factors, [1,5,25])
+
+    def test_get_factors_6(self):
+        factors=mathkit.get_factors(8, True)
+        factors.sort()
+        self.assertEqual(factors, [2,4])
+
+    def test_get_factors_7(self):
+        factors=mathkit.get_factors(25, True)
+        factors.sort()
+        self.assertEqual(factors, [5])
+
+    def test_fibonacci_1(self):
+        fibonacci_nums=mathkit.fib_seq(10)
+        self.assertEqual(fibonacci_nums, [1, 2, 3, 5, 8, 13, 21, 34, 55, 89])
 
 if __name__ == '__main__':
     unittest.main()
