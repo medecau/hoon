2b30eedc833182e46ad33c77f20cedf623d7634a
Pedro Rodrigues <medecau@gmail.com>
2012-06-14 21:23:20 +0100

    prepping up to release

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python
-
 from distutils.core import setup
 
 setup(name='worm',
-      version='0.1',
+      version='0.2',
+      py_modules=['worm',],
       description='A collection of very common code patterns in Python with nice defaults.',
       author='Pedro Rodrigues',
       author_email='medecau@gmail.com',
       url='https://github.com/medecau/worm',
-      packages=['worm',]
      )
--- worm.py
+++ worm.py
@@ -10,18 +10,25 @@ def cute(t):
     return json.dumps(t, indent=2)
 
 def translate(s, old, new):
-    '''Translate each character in old to the character at the same position in new'''
+    '''
+        Translate each character in old to the character
+        at the same positionin new
+    '''
     return string.translate(s, string.maketrans(old, new))
 
 def to_ascii(s):
-    '''Force a string to convert to ASCII. non-ASCII characters will be droped'''
+    '''
+        Force a string to convert to ASCII.
+        non-ASCII characters will be droped
+    '''
     try:
         return s.decode('ascii', 'ignore')
     except:
         return s.encode('ascii', 'ignore').decode('ascii','ignore')
 def read(file, create=False):
-    '''Read file contents.
-    Parameter create sets wether to create a new file or not.'''
+    '''
+        Read file contents.
+        Parameter create sets wether to create a new file or not.'''
     if create:
         mode = 'r+'
     else:
@@ -29,8 +36,10 @@ def read(file, create=False):
     return open(file, mode).read()
 
 def write(file, arg, create=True):
-    '''Write contents to file.
-    Parameter create sets wether to create a new file or not.'''
+    '''
+        Write contents to file.
+        Parameter create sets wether to create a new file or not.
+    '''
     if create:
         mode = 'w+'
     else:
@@ -38,8 +47,10 @@ def write(file, arg, create=True):
     open(file, mode).write(arg)
 
 def append(file, arg, create=True):
-    '''Write contents to file.
-    Parameter create sets wether to create a new file or not.'''
+    '''
+        Write contents to file.
+        Parameter create sets wether to create a new file or not.
+    '''
     if create:
         mode = 'a+'
     else:
@@ -47,26 +58,37 @@ def append(file, arg, create=True):
     open(file,'a+').write(arg)
 
 def sm(a=None, b=None, isjunk=None):
-    '''Short for the SequenceMatcher constructor. isjunk is moved to the right
-    so it\'s not required when providing a and b parameters.'''
+    '''
+        Short for the SequenceMatcher constructor.
+        isjunk is moved to the right so it\'s not required
+        when providing a and b parameters.
+    '''
     import difflib
     return difflib.SequenceMatcher(isjunk, a, b)
 
 def ratio(a, b):
-    '''Return a measure of the sequences' similarity.'''
+    '''
+        Return a measure of the sequences' similarity.
+    '''
     return sm(a, b).ratio()
 
 def qratio(a, b):
-    '''Return an upper bound on ratio() very quickly.'''
+    '''
+        Return an upper bound on ratio() very quickly.
+    '''
     return sm(a, b).real_quick_ratio()
 
 def get_opcodes(a, b):
-    '''Get a list of tuples describing how to turn a into b.'''
+    '''
+        Get a list of tuples describing how to turn a into b.
+    '''
     return sm(a, b).get_opcodes()
 
 def qmatch(a, b, ratio=1):
-    '''Fast test the similarity of two sequences by testing with
-    real_quick_ratio() first.'''
+    '''
+        Fast test the similarity of two sequences by testing with
+        real_quick_ratio() first.
+    '''
     smo = sm(a, b)
     if smo.real_quick_ratio()<ratio:
         return False
@@ -77,24 +99,30 @@ def qmatch(a, b, ratio=1):
             return True
 
 def hash(algorithm='sha1'):
-    '''Generic constructor. algorithm parameter defaults to sha1.
-    Returns hash object.'''
+    '''
+        Generic constructor. algorithm parameter defaults to sha1.
+        Returns hash object.
+    '''
     return hashlib.new(algorithm)
 
 def digest(s, algorithm='sha1'):
-    '''Return the digest of the string passed in arg. This string may contain
-    non-ASCII characters, including null bytes.
+    '''
+        Return the digest of the string passed in arg. This string may contain
+        non-ASCII characters, including null bytes.
 
-    algorithm parameter defaults to sha1.'''
+        algorithm parameter defaults to sha1.
+    '''
     h = hashlib.new(algorithm)
     h.update(s)
     return h.digest()
 
 def hexdigest(s, algorithm='sha1'):
-    '''Like digest() except the digest is returned as a string of double
-    length, containing only hexadecimal digits.
+    '''
+        Like digest() except the digest is returned as a string of double
+        length, containing only hexadecimal digits.
 
-    algorithm parameter defaults to sha1.'''
+        algorithm parameter defaults to sha1.
+    '''
     h = hashlib.new(algorithm)
     h.update(s)
     return h.hexdigest()
@@ -119,6 +147,9 @@ class Fibonacci(object):
         return self.o
 
 def fib_seq(num):
+    '''
+        Returns the fibonacci sequence in a list up to num
+    '''
     seq=[]
     f=Fibonacci()
     for i in range(num):
@@ -126,6 +157,9 @@ def fib_seq(num):
     return seq
 
 def is_prime(num):
+    '''
+        Checks for the primality of a number
+    '''
     from math import ceil
     from math import sqrt
     if (num%2 == 0 and num>2) or num < 2 or type(num) is not int:
@@ -138,6 +172,9 @@ def is_prime(num):
         return True
 
 def next_prime(num):
+    '''
+        Returns the next prime
+    '''
     if num > 2:
         if num%2==0:
             num-=1
@@ -155,6 +192,9 @@ def next_prime(num):
         return 2
 
 def get_factors(num, short=False):
+    '''
+        Calculates the factors of num
+    '''
     from math import ceil
     from math import sqrt
     factors=[]
@@ -172,8 +212,8 @@ def get_factors(num, short=False):
 
 def obj(name='anonymous'):
     '''
-    Returns a new object
-    TODO: change the type and allow for isinstance() to work properly
+        Returns a new object
+        TODO: change the type and allow for isinstance() to work properly
     '''
     return type(str(name), (object, ), {})
 
