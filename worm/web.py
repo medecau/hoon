ae277dbbd27856ae9ee6968899aa7e03bf3edf84
Pedro Rodrigues <medecau@gmail.com>
2012-06-14 18:52:35 +0100

    new implementation of worm.web

--- worm/web.py
+++ worm/web.py
@@ -1,4 +1,12 @@
-def readurl(url):
-    '''Return URL contents.'''
-    import urllib2
-    return urllib2.urlopen(url).read()
+import urllib
+
+def request(url, data):
+    """
+        Makes a simple request. If no data it's a GET else it's a POST.
+        Returns a string.
+    """
+    if data is not None:
+        post_data = urllib.urlencode(data)
+    else:
+        post_data = None
+    return urllib.urlopen(url, post_data).read()
