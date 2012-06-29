877637f02272b54dad8c7ef592048647a7f5e483
Pedro Rodrigues <medecau@gmail.com>
2012-06-29 09:01:46 +0100

    a better description for 0.3.0

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(name='worm',
-      version='0.2',
+      version='0.3.0',
       py_modules=['worm',],
-      description='A collection of very common code patterns in Python with nice defaults.',
+      description='Python boilerplate with nice defaults.',
       author='Pedro Rodrigues',
       author_email='medecau@gmail.com',
       url='https://github.com/medecau/worm',
